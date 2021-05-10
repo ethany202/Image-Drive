ce0cac58a9969a36aa87809f70b66b6a8e6267c3
Ethan Ye <ethan.ye0312@gmail.com>
2021-05-09 21:57:52 -0400

    Added new files

---
+++ homeScript.js
@@ -0,0 +1,13 @@
+var loginButton = document.getElementById("login-button");
+var signupButton = document.getElementById("sign-up-button");
+
+
+function login(){
+    console.log("alksjklja");
+    window.location.href = "loginRedirect.html";
+};
+
+function signup(){
+    console.log("jlakjdl");
+    window.location.href = "signupRedirect.html";
+}
